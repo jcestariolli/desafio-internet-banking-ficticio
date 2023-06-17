b98b4f2e812304e718deb4c879118ae472ad7e57
J Cestariolli <jessica.cestariolli@gmail.com>
2023-06-17 18:52:02 -0300

    upd AccountDto.java del MoneyDeserializer.java

--- app/src/main/java/internetbankingficticio/dto/account/AccountDto.java
+++ app/src/main/java/internetbankingficticio/dto/account/AccountDto.java
@@ -2,10 +2,8 @@ package internetbankingficticio.dto.account;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import internetbankingficticio.dto.AbstractInternetBankingDto;
-import internetbankingficticio.mapper.utils.MoneyDeserializer;
 import internetbankingficticio.mapper.utils.MoneySerializer;
 import lombok.*;
 
@@ -24,7 +22,6 @@ public class AccountDto extends AbstractInternetBankingDto {
 
     @JsonProperty(value = "saldo", required = true)
     @JsonSerialize(using = MoneySerializer.class)
-    @JsonDeserialize(using = MoneyDeserializer.class)
     private BigDecimal balance;
 
     @JsonProperty(value = "plano_exclusive", defaultValue = "false")
