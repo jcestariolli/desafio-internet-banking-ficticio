cee45f9c02e1b7f102f49e1f0598c56b79cc0d48
J Cestariolli <jessica.cestariolli@gmail.com>
2023-06-17 09:57:00 -0300

    add initial maven project

---
+++ app/src/test/java/internetbankingficticio/InternetBankingFicticioApplicationTests.java
@@ -0,0 +1,13 @@
+package internetbankingficticio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetBankingFicticioApplicationTests extends AbstractTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
