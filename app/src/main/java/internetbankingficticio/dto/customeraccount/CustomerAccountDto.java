b622b9f7bbce3f39aacb9ff79f32c6e2989ee528
J Cestariolli <jessica.cestariolli@gmail.com>
2023-06-17 19:10:25 -0300

    add CustomerAccount dtos

---
+++ app/src/main/java/internetbankingficticio/dto/customeraccount/CustomerAccountDto.java
@@ -0,0 +1,21 @@
+package internetbankingficticio.dto.customeraccount;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import internetbankingficticio.dto.AbstractInternetBankingDto;
+import internetbankingficticio.dto.account.AccountDto;
+import internetbankingficticio.dto.customer.CustomerDto;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CustomerAccountDto extends AbstractInternetBankingDto {
+    @JsonProperty(value = "cliente", required = true)
+    CustomerDto customerDto;
+    @JsonProperty(value = "conta", required = true)
+    AccountDto accountDto;
+}
