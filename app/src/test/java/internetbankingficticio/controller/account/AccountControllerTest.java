8408decf4f52bcf85fa3324385498c58c37c965e
J Cestariolli <jessica.cestariolli@gmail.com>
2023-06-17 18:06:01 -0300

    add AccountController + tests

---
+++ app/src/test/java/internetbankingficticio/controller/account/AccountControllerTest.java
@@ -0,0 +1,122 @@
+package internetbankingficticio.controller.account;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import internetbankingficticio.dto.account.AccountDto;
+import internetbankingficticio.service.account.AccountServiceIF;
+import internetbankingficticio.test.AbstractTest;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+
+import static internetbankingficticio.test.utils.TestUtils.*;
+import static internetbankingficticio.test.utils.account.AccountObjectsTestUtils.*;
+import static internetbankingficticio.test.utils.account.AccountServiceMockTestUtils.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AccountController.class)
+public class AccountControllerTest extends AbstractTest {
+
+    private static final String API_ENDPOINT = "/contas";
+    private static ObjectMapper MAPPER;
+    @Autowired
+    private MockMvc mvc;
+    @MockBean
+    private AccountServiceIF accountService;
+
+    @BeforeAll
+    public static void configureObjectMapperBeforeTests() {
+        MAPPER = configureObjectMapper();
+    }
+
+    @Test
+    @DisplayName("Should return Status Ok (200) And AccountDtoList when listAllAccounts API finds at least one Account")
+    public void shouldReturnStatusOkAndAccountDtoList_whenListAllAccountsFindsAccount() throws Exception {
+        List<AccountDto> expectedAccountDtoList = generateAccountDtoListObject();
+        mockServiceListAllAccountsWithAccountList(accountService, expectedAccountDtoList);
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(API_ENDPOINT).accept(MediaType.APPLICATION_JSON)).andDo(print()).andExpect(status().isOk()).andReturn();
+
+        List<AccountDto> returnedAccountDtoList = parseResponse(MAPPER, mvcResult, new TypeReference<List<AccountDto>>() {});
+        assertThat(returnedAccountDtoList).isNotEmpty();
+        assertThat(returnedAccountDtoList).size().isEqualTo(expectedAccountDtoList.size());
+        assertEquals(returnedAccountDtoList, expectedAccountDtoList);
+    }
+
+    @Test
+    @DisplayName("Should return Status NoContent (204) when listAllAccounts API does not find any Account")
+    public void shouldReturnStatusNoContent_whenListAllAccountsApiDoesNotFindAnyAccount() throws Exception {
+        mockServiceListAllAccountsWithAccountList(accountService, new ArrayList<>());
+        mvc.perform(MockMvcRequestBuilders.get(API_ENDPOINT).accept(MediaType.APPLICATION_JSON)).andDo(print()).andExpect(status().isNoContent()).andReturn();
+    }
+
+    @Test
+    @DisplayName("Should return Status Ok (200) And AccountDto when findAccountById API finds a Account")
+    public void shouldReturnStatusOkAndAccountDto_whenFindAccountByIdApiFindsAccount() throws Exception {
+        String accountId = "12345678";
+        AccountDto expectedAccountDto = generateAccountDtoObject(accountId, BigDecimal.valueOf(100).setScale(2, RoundingMode.HALF_UP), true);
+        mockServiceFindAccountByIdWithAccount(accountService, accountId, expectedAccountDto);
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(API_ENDPOINT + "/" + accountId).accept(MediaType.APPLICATION_JSON)).andDo(print()).andExpect(status().isOk()).andReturn();
+
+        AccountDto returnedAccountDto = parseResponse(MAPPER, mvcResult, AccountDto.class);
+        assertThat(returnedAccountDto).isEqualTo(expectedAccountDto);
+    }
+
+    @Test
+    @DisplayName("Should return Status NotFound (404) when findAccountById API does not find the Resource")
+    public void shouldReturnStatusNotFound_whenFindAccountByIdApiDoesNotFindTheResource() throws Exception {
+        String accountId = "12345678";
+        mockServiceFindAccountByIdWithEmptyResult(accountService, accountId);
+        mvc.perform(MockMvcRequestBuilders.get(API_ENDPOINT + "/" + accountId).accept(MediaType.APPLICATION_JSON)).andDo(print()).andExpect(status().isNotFound()).andReturn();
+    }
+
+    @Test
+    @DisplayName("Should return Status Created (201) when createAccount API")
+    public void shouldReturnStatusCreated_whenCreateAccountApi() throws Exception {
+        AccountDto expectedAccountDto = generateAccountDtoObject("12345678", BigDecimal.valueOf(100).setScale(2, RoundingMode.HALF_UP), true);
+        mockServiceCreateAccountWithAccount(accountService, expectedAccountDto);
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(API_ENDPOINT).content(asJsonString(MAPPER, generateAccountCreateDtoObject("12345678", BigDecimal.valueOf(100).setScale(2, RoundingMode.HALF_UP), true))).contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)).andDo(print()).andExpect(status().isCreated()).andReturn();
+
+        AccountDto returnedAccountDto = parseResponse(MAPPER, mvcResult, AccountDto.class);
+        assertThat(returnedAccountDto).isEqualTo(expectedAccountDto);
+    }
+
+    @Test
+    @DisplayName("Should return Status Ok (200) when updateAccount API")
+    public void shouldReturnStatusOk_whenUpdateAccountApi() throws Exception {
+        String accountId = "12345678";
+        AccountDto expectedAccountDto = generateAccountDtoObject(accountId, BigDecimal.valueOf(100).setScale(2, RoundingMode.HALF_UP), true);
+        mockServiceUpdateAccountWithAccount(accountService, expectedAccountDto);
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.put(API_ENDPOINT + "/" + accountId).content(asJsonString(MAPPER, generateAccountUpdateDtoObject(true))).contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)).andDo(print()).andExpect(status().isOk()).andReturn();
+
+        AccountDto returnedAccountDto = parseResponse(MAPPER, mvcResult, AccountDto.class);
+        assertThat(returnedAccountDto).isEqualTo(expectedAccountDto);
+    }
+
+    @Test
+    @DisplayName("Should return Status NotFound (404) when updateAccount API does not find the Resource")
+    public void shouldReturnStatusNotFound_whenUpdateAccountApiDoesNotFindTheResource() throws Exception {
+        String accountId = "12345678";
+        mockServiceUpdateAccountWithEmptyResult(accountService, accountId, generateAccountUpdateDtoObject(true));
+        mvc.perform(MockMvcRequestBuilders.put(API_ENDPOINT + "/" + accountId).content(asJsonString(MAPPER, generateAccountUpdateDtoObject(true))).contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)).andDo(print()).andExpect(status().isNotFound()).andReturn();
+
+    }
+}
--- app/src/test/java/internetbankingficticio/test/utils/account/AccountObjectsTestUtils.java
+++ app/src/test/java/internetbankingficticio/test/utils/account/AccountObjectsTestUtils.java
@@ -6,13 +6,14 @@ import internetbankingficticio.dto.account.AccountDto;
 import internetbankingficticio.dto.account.AccountUpdateDto;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 
 public class AccountObjectsTestUtils {
 
 
     public static List<AccountDao> generateAccountDaoListObject() {
-        return List.of(generateAccountDaoObject("12345678", new BigDecimal(100), true), generateAccountDaoObject("0101010101", new BigDecimal("1.10"), false), generateAccountDaoObject("99999999", new BigDecimal(200), true));
+        return List.of(generateAccountDaoObject("12345678", BigDecimal.valueOf(100).setScale(2, RoundingMode.HALF_UP), true), generateAccountDaoObject("0101010101", new BigDecimal("1.10"), false), generateAccountDaoObject("99999999", new BigDecimal(200), true));
     }
 
     public static AccountDao generateAccountDaoObject(String id, BigDecimal balance, boolean exclusivePlan) {
@@ -20,7 +21,7 @@ public class AccountObjectsTestUtils {
     }
 
     public static List<AccountDto> generateAccountDtoListObject() {
-        return List.of(generateAccountDtoObject("12345678", new BigDecimal(100), true), generateAccountDtoObject("0101010101", new BigDecimal("1.10"), false), generateAccountDtoObject("99999999", new BigDecimal(200), true));
+        return List.of(generateAccountDtoObject("12345678", BigDecimal.valueOf(100).setScale(2, RoundingMode.HALF_UP), true), generateAccountDtoObject("0101010101", BigDecimal.valueOf(1.1).setScale(2, RoundingMode.HALF_UP), false), generateAccountDtoObject("99999999", BigDecimal.valueOf(.01).setScale(2, RoundingMode.HALF_UP), true));
     }
 
     public static AccountDto generateAccountDtoObject(String id, BigDecimal balance, boolean exclusivePlan) {
